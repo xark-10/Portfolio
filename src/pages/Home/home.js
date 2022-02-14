a2bfc561b2afa9a8913b972f4591c3e242866add
xark-10 <84316716+xark-10@users.noreply.github.com>
2022-02-14 23:57:25 +0530

    Home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import homeImg from '../../Assets/031.png'
 
 
 function Home(){
     return(
-    <div className="container">
-        <h1 className="text-center" style={{paddingTop:'30%'}}>
-        Home
-        </h1>      
+    <div className="container col-lg-12">
+        <img src={homeImg} className="homeImg"/>
     </div>
     )
 }

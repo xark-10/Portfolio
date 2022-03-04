039f9dc571d5ace746261dad2173019af8165270
xark-10 <84316716+xark-10@users.noreply.github.com>
2022-03-05 00:55:48 +0530

    changes

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,9 +5,13 @@ import homeImg from '../../Assets/031.png'
 
 function Home(){
     return(
-    <div className="container col-lg-12">
+    <div className="container col-lg-12 c-preview__img">
         <img src={homeImg} className="homeImg"/>
     </div>
+
+    
+
+    
     )
 }
 
